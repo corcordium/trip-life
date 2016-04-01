c1bdc6e1491352fd299c75b08cb4482029b09635
corcordium <fyrchina@hotmail.com>
2016-04-01 09:31:47 +0800

    first git

---
+++ js/fontSize.js
@@ -0,0 +1,18 @@
+function bigSize(){
+	oP = document.getElementsByTagName('p');
+	for(var i=0;i<oP.length;i++){
+		oP[i].style.fontSize = 16 + 'px';
+	}
+}
+function middleSize(){
+	oP = document.getElementsByTagName('p');
+	for(var i=0;i<oP.length;i++){
+		oP[i].style.fontSize = 14 + 'px';
+	}
+}
+function smallSize(){
+	oP = document.getElementsByTagName('p');
+	for(var i=0;i<oP.length;i++){
+		oP[i].style.fontSize = 12 + 'px';
+	}
+}
